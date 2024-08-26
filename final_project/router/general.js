b7d8e312ccf404ab1c3b7a22484a5c9a341c40bf
rom-code <raad.cars@gmail.com>
2024-08-26 14:53:25 -0400

    Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,17 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn= req.params.isbn;
+  const book = books(isbn);
+    if (book){
+        return res.status(200).json(book);
+        
+    } else {
+        return res.status(404).json({message: "book ton found"})
+    }
+// send filtered_books array as responce to the client
+  //res.send("User " + req.params.id + " last successful login " + Date());
+  //return res.status(300).json({message: "Book not found"});
  });
   
 // Get book details based on author
